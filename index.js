de2ff4563be6df3be9caa91b0b8bf6a8447810f4
eduzgun <emreduz00@gmail.com>
2023-07-23 16:59:36 +0100

    created main game loop

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+const prompt = require('prompt-sync')();
 /*
 code will go here
 */
+
+const checkWinner = (a, b) => {
+  if (a == undefined || b == undefined) {
+    throw "This function requires two arguments!";
+  } else if (typeof a != "string" || typeof b != "string") {
+    throw "Arguments must be strings!";
+  } else {
+    if ((a == "rock" && b == "scissors") || (a == "scissors" && b == "paper") || (a == "paper" && b == "rock")) {
+      return a; // a wins
+    } else if ((b == "rock" && a == "scissors") || (b == "scissors" && a == "paper") || (b == "paper" && a == "rock")) {
+      return b; // b wins
+    } else {
+      return "draw"; // a and b are the same or invalid inputs
+    }
+  }
+};
+
+const getComputerChoice = () => {
+  const arr = ["rock", "paper", "scissors"];
+  const computerChoice = arr[Math.floor(Math.random() * arr.length)];
+  return computerChoice
+}
+
+const getUserChoice = () => {
+  let userChoice = prompt('Choose: rock(r), paper(p) or scissors(s)? :   ');
+  const accepted = ["rock", "paper", "scissors", "r", "p", "s"]
+  while (!accepted.includes(userChoice)) {
+    userChoice = prompt('Choose: rock(r), paper(p) or scissors(s)? :   ');
+  }
+  if (userChoice == "r"){
+    userChoice = "rock";
+  } else if (userChoice == "p"){
+    userChoice = "paper";
+  } else if (userChoice = "s") {
+    userChoice = "scissors";
+  } else {
+    //pass
+  }
+  return userChoice;
+}
+const mainLoop = () => {
+  console.log("Welcome to rock paper scissors in Javascript!");
+  const userChoice = getUserChoice();
+  const computerChoice = getComputerChoice();
+  console.log(checkWinner(userChoice, computerChoice));
+}
+
+mainLoop();
+module.exports = { checkWinner };
--- index.test.js
+++ index.test.js
@@ -1,38 +1,23 @@
+const each = require('jest-each').default;
+const { checkWinner } = require("./index.js");
 
-describe ("gameLoop", () => {
-    test("exists", () => {
-        expect(gameLoop).toBeDefined();
-    })
-
-    test("is a function", () => {
-        expect(findLargest instanceof Function).toEqual(true);
-    })
-})
-
-describe ("checkWinner", () => {
+describe("checkWinner", () => {
     test("exists", () => {
         expect(checkWinner).toBeDefined();
-    })
+    });
 
     test("is a function", () => {
         expect(checkWinner instanceof Function).toEqual(true);
-    })
-
-    test("exists", () => {
-        expect(findLargest).toBeDefined();
-    })
+    });
 
     it("Throws an error when passed no values", () => {
         expect(() => checkWinner()).toThrow("This function requires two arguments!");
-    })
+    });
 
-    it("Throws an error when passed non-string arguments", () => {
-        expect(() => getLonger(3, 6)).toThrow("Arguments must be strings!");
-    })
 
     it("Throws an error when passed only one value", () => {
-        expect(() => getLonger("red")).toThrow("This function requires two arguments!");
-    })
+        expect(() => checkWinner("rock")).toThrow("This function requires two arguments!");
+    });
 
     each([
         ["rock", "rock", "scissors"],
@@ -43,6 +28,5 @@ describe ("checkWinner", () => {
         ["draw", "paper", "paper"]
     ]).test(`Returns %s when passed %s and %s`, (expected, a, b) => {
         expect(checkWinner(a, b)).toBe(expected);
-    })
-
-})
+    });
+});
